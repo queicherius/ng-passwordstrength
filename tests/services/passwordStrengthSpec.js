5ff9380e65f8c93b9c017e114aaf87f81b8589c9
queicherius <queicherius@gmx.de>
2013-12-22 22:45:05 +0100

    rewritten statements to be more clear

--- tests/services/passwordStrengthSpec.js
+++ tests/services/passwordStrengthSpec.js
@@ -16,14 +16,14 @@ describe("Unit-Tests: passwordStrength service", function () {
 
     describe("Additions", function () {
 
-        it('should count characters', function () {
+        it('should give points for characters', function () {
 
             var strength = passwordStrengthService.characters("12345");
             expect(strength).toEqual(5 * 4);
 
         });
 
-        it('should count uppercase characters', function () {
+        it('should give points for uppercase characters', function () {
 
             // Note: the first uppercase character doesn't count
             var strength = passwordStrengthService.uppercaseCharacters("AaAaaAa");
@@ -31,38 +31,38 @@ describe("Unit-Tests: passwordStrength service", function () {
 
         });
 
-        it('should count lowercase characters', function () {
+        it('should give points for lowercase characters', function () {
 
             var strength = passwordStrengthService.lowercaseCharacters("aaAaAAaAa");
             expect(strength).toEqual((9 - 5) * 2);
 
         });
 
-        it('should count numbers', function () {
+        it('should give points for numbers', function () {
 
             var strength = passwordStrengthService.numbers("0aa1Aa2AAa3Aa456789");
             expect(strength).toEqual(10 * 4);
 
         });
 
-        it('should count symbols', function () {
+        it('should give points for symbols', function () {
 
             var strength = passwordStrengthService.symbols("a%$4%&§");
             expect(strength).toEqual(5 * 6);
 
         });
 
-        it('should count middleNumberOrSymbols', function () {
+        it('should give points for middle numbers or symbols', function () {
 
-            var strength = passwordStrengthService.middleNumberOrSymbols("$a%$%&§");
+            var strength = passwordStrengthService.middleNumbersOrSymbols("$a%$%&§");
             expect(strength).toEqual(4 * 2);
 
-            var strength = passwordStrengthService.middleNumberOrSymbols("12345");
+            var strength = passwordStrengthService.middleNumbersOrSymbols("12345");
             expect(strength).toEqual(3 * 2);
 
         });
 
-        it('should count requirements', function () {
+        it('should give points for requirements', function () {
 
             // 2 matched -> need 4 to count
             var strength = passwordStrengthService.requirements("abcdefgh");
@@ -83,7 +83,7 @@ describe("Unit-Tests: passwordStrength service", function () {
 
     describe("Deductions", function () {
 
-        it('should deduct for lettersOnly', function () {
+        it('should deduct points for passwords with only letters', function () {
 
             var strength = passwordStrengthService.lettersOnly("aaaaaaaaaa");
             expect(strength).toEqual(-10);
@@ -93,7 +93,7 @@ describe("Unit-Tests: passwordStrength service", function () {
 
         });
 
-        it('should deduct for numbersOnly', function () {
+        it('should deduct points for passwords with numbers only', function () {
 
             var strength = passwordStrengthService.numbersOnly("1234567890");
             expect(strength).toEqual(-10);
@@ -103,32 +103,32 @@ describe("Unit-Tests: passwordStrength service", function () {
 
         });
 
-        it('should deduct for repeatCharacters', function () {
-            
+        it('should deduct points for repeat characters', function () {
+
             var strength = passwordStrengthService.repeatCharacters("aa");
             expect(strength).toEqual(-4);
-            
+
             var strength = passwordStrengthService.repeatCharacters("aaa");
             expect(strength).toEqual(-14);
-            
+
             var strength = passwordStrengthService.repeatCharacters("aaabbaaa");
             expect(strength).toEqual(-50);
-            
+
             var strength = passwordStrengthService.repeatCharacters("aaaaaaaaaaaaaa");
             expect(strength).toEqual(-150);
-            
+
             var strength = passwordStrengthService.repeatCharacters("9979979799");
             expect(strength).toEqual(-51);
-            
+
             var strength = passwordStrengthService.repeatCharacters("9999999");
             expect(strength).toEqual(-60);
-            
+
             var strength = passwordStrengthService.repeatCharacters("%%%");
             expect(strength).toEqual(-14);
-            
+
         });
 
-        it('should deduct for consecutiveUppercaseCharacters', function () {
+        it('should deduct points for consecutive uppercase characters', function () {
 
             var strength = passwordStrengthService.consecutiveUppercaseCharacters("aaAABA1%ccddDDDeeEhhHH"),
                 count = (4 - 1) + (3 - 1) + (2 - 1);
@@ -137,7 +137,7 @@ describe("Unit-Tests: passwordStrength service", function () {
 
         });
 
-        it('should deduct for consecutiveLowercaseCharacters', function () {
+        it('should deduct points for consecutive lowercase characters', function () {
 
             var strength = passwordStrengthService.consecutiveLowercaseCharacters("aaAABA1%ccddDDDeeEhhHHh"),
                 count = (2 - 1) + (4 - 1) + (2 - 1) + (2 - 1);
@@ -146,7 +146,7 @@ describe("Unit-Tests: passwordStrength service", function () {
 
         });
 
-        it('should deduct for consecutiveNumbers', function () {
+        it('should deduct points for consecutive numbers', function () {
 
             var strength = passwordStrengthService.consecutiveNumbers("123aa45687aabb564%%asd55"),
                 count = (3 - 1) + (5 - 1) + (3 - 1) + (2 - 1);
@@ -155,56 +155,56 @@ describe("Unit-Tests: passwordStrength service", function () {
 
         });
 
-        it('should deduct for sequentialCharacters', function () {
+        it('should deduct points for sequential characters', function () {
 
             // Alphabetical order
             var strength = passwordStrengthService.sequentialCharacters("abc");
             expect(strength).toEqual(-(1 * 6));
-            
+
             var strength = passwordStrengthService.sequentialCharacters("cba");
             expect(strength).toEqual(-(1 * 6));
-            
+
             // Alphabet order is deducted double when leetspeek is involved
             var strength = passwordStrengthService.sequentialCharacters("abcd");
             expect(strength).toEqual(-((2 + 1) * 6));
-            
+
             var strength = passwordStrengthService.sequentialCharacters("dcba");
             expect(strength).toEqual(-((2 + 1) * 6));
-            
+
             // Keyboard order are deducted double
             var strength = passwordStrengthService.sequentialCharacters("qwert");
             expect(strength).toEqual(-((3 + 3) * 6));
 
             var strength = passwordStrengthService.sequentialCharacters("trewq");
             expect(strength).toEqual(-((3 + 3) * 6));
-            
+
         });
 
-        it('should deduct for sequentialNumbers', function () {
+        it('should deduct points for sequential numbers', function () {
 
             var strength = passwordStrengthService.sequentialNumbers("123");
             expect(strength).toEqual(-(1 * 6));
-            
+
             var strength = passwordStrengthService.sequentialNumbers("098");
             expect(strength).toEqual(-(1 * 6));
-            
+
         });
 
-        it('should deduct for sequentialSymbols', function () {
+        it('should deduct points for sequential symbols', function () {
 
             var strength = passwordStrengthService.sequentialSymbols("@#$");
             expect(strength).toEqual(-(1 * 2));
-            
+
             var strength = passwordStrengthService.sequentialSymbols("$#@");
             expect(strength).toEqual(-(1 * 2));
-            
+
         });
 
-        it('should deduct for dictionary', function () {
+        it('should deduct points for words in the dictionary', function () {
 
             var strength = passwordStrengthService.dictionary("merlin");
             expect(strength).toEqual(-Infinity);
-            
+
         });
 
     });
@@ -213,10 +213,10 @@ describe("Unit-Tests: passwordStrength service", function () {
 
         var strength = passwordStrengthService.bounds(-10);
         expect(strength).toEqual(0);
-        
+
         var strength = passwordStrengthService.bounds(110);
         expect(strength).toEqual(100);
-        
+
     });
 
 });
